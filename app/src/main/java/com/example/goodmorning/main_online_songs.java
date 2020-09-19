32cbc89d422492df5bd8941d58792ca6da4f9a44
Ashish2000L <ashishkumar28april@gmail.com>
2020-09-19 17:21:21 +0530

    .

---
+++ app/src/main/java/com/example/goodmorning/main_online_songs.java
@@ -0,0 +1,17 @@
+package com.example.goodmorning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_online_songs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_online_songs);
+
+
+
+    }
+}
